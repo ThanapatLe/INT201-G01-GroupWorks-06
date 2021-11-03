eb0fce461dbad742d1f1716a3259fe2b093fbb94
kanphong <53311303+kanphong@users.noreply.github.com>
2021-11-03 15:43:47 +0700

    ยากจัง T_T

--- assignment6/search.js
+++ assignment6/search.js
@@ -1,4 +1,9 @@
 import { products } from "./product.js"; 
 
-let pic = document.createElement("img");
-  pic.setAttribute("src","");
+const Pro = document.querySelector("#product")
+let searhicon = document.createElement("img");
+let v = document.createElement("a");
+searhicon.setAttribute("src","search.png")
+searhicon.setAttribute("class", "sh")
+document.body.append(searhicon)
+
