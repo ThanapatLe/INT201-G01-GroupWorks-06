9ea0a5b9733ae60a33a577a8b7ee297aa22e4de5
kanphong <53311303+kanphong@users.noreply.github.com>
2021-11-07 00:37:56 +0700

    มุแง้~~~~

--- assignment6/search.js
+++ assignment6/search.js
@@ -1,7 +1,25 @@
+import {products} from './product.js';
+let icon = document.querySelector("#search-icon");
+let form = document.querySelector('#form-search');
+let btn_search = document.querySelector('#search');
+let text = document.querySelector('#text-search');
+icon.addEventListener("click", toggle);
+btn_search.addEventListener("click",console.log(text.value));
+let is_click = false;
+function toggle() {
+  is_click = !is_click;
+  if (is_click) {
+    form.setAttribute("class","visible");
+  } else {
+    form.setAttribute("class",'invisible');
+  }
+}
+function search(text){
+    if(text.length != 0){
+        let item = products.filter((p)=> p.productName.toLowerCase().includes(text.toLowerCase()));
+        console.log(item);
+    }
+    console.log(item);
+}
 
-let icon = document.querySelector("#search");
-let text = document.createElement("input");
-text.setAttribute("type", "text");
-let buttom = document.createElement("button");
-buttom.innerHTML = "<i>OK</i>";
-icon.after(text,buttom);
+
